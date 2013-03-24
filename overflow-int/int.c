bacd21c414dc1d077cb735cf998881adc1d0f2ff
Glaydston Veloso <glaydston.oliveira@gmail.com>
2013-03-24 16:27:59 -0300

    Overflow of Integer

---
+++ overflow-int/int.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+	int a = 32766;
+	int b = 1;
+
+	printf("\n%d + %d é: %d\n\n", a,b, a+b);
+	
+}
---
+++ overflow-int/overflow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+	int a = 2147483646;
+	int b = 10;
+	int c = a + b;
+	
+	printf("\n\n%d + %d é: %d\n\n", a,b,c);
+
+}
+
